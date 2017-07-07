f1572305affbed8b3d0089ca1c1742612e445c10
code_chen <1909916440@qq.com>
2017-07-07 20:41:06 +0800

    更新服务工作线程，设置删除不需要的旧缓存

--- ServiceWorker/sw.js
+++ ServiceWorker/sw.js
@@ -17,7 +17,7 @@ self.addEventListener('install', function(events){
 //缓存和返回 请求
 self.addEventListener('fetch', function(event){
 	event.respondWith(
-		cache.match(event.request)
+		caches.match(event.request)
 			.then(function(response){
 				if(response){
 					return response;
@@ -25,4 +25,46 @@ self.addEventListener('fetch', function(event){
 				return fetch(event.request);
 			})
 	);
+});
+
+//连续缓存新请求
+self.addEventListener('fetch', function(event){
+	event.respondWith(
+		caches.match(event.request)
+			.then(function(response){
+				if(response){
+					return response;
+				}
+				var req = events.request.clone();
+				return fetch(req)
+					.then(function(response){
+						if(!response || response.status!== 200 ||response.type !== 'basic'){
+							return response;
+						}
+						var resToCache = response.clone();
+						caches.open(CACHE_NAME)
+							.then(function(cache){
+								cache.put(event.request, resToCache);
+							});
+						return response;
+					});
+			}));
+});
+/** 
+* 旧服务工作线程被终止，新服务工作线程取得控制权 触发 activate 事件
+* 遍历服务工作线程的所有缓存，并删除不在白名单中的缓存
+* 如果想要废弃旧服务线程的旧缓存，可以在白名单去掉
+*/
+self.addEventListener('activate', function(event){
+	var cacheWhitelist = ['page-cache-1', 'blog-posts-cache-v1'];
+	event.waitUntil(
+		caches.keys().then(function(cacheNames){
+			return Promise.all(
+				cacheNames.map(function(cacheName){
+					if(cacheWhitelist.indexOf(cacheName === -1)){
+						return caches.delete(cacheName);
+					}
+				})
+			);
+		}));
 });
